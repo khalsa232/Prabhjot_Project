cffc4a8e19103c2b064a540edd5c53c8a3dbcb8e
khalsa232 <prabhjotk14@nycstudents.net>
2024-10-30 10:58:21 -0400

    uploading files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function guessAdd1(){
+    document.getElementById("guess-el").innerText += 1;
+    //document.getElementById("guess-el").innerText = element;
+}
+function guessAdd2(){
+    document.getElementById("guess-el").innerText += 2;
+    //document.getElementById("guess-el").innerText = element;
+}
+function guessAdd3(){
+    document.getElementById("guess-el").innerText += 3;
+    //document.getElementById("guess-el").innerText = element;
+}
