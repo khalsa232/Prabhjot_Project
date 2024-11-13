d6d6f49491b911be74acb64b10183125a6d6ff5d
khalsa232 <prabhjotk14@nycstudents.net>
2024-11-13 00:49:37 -0500

    1.1

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
+let guessCounter = Number(localStorage.getItem("guessCounter")) || 7;
+document.getElementById("counter-el").innerText = guessCounter;
 
-function guessAdd1() {
-    document.getElementById("guess-el").innerText += 1;
-    //document.getElementById("guess-el").innerText = element;
-}
-function guessAdd2() {
-    document.getElementById("guess-el").innerText += 2;
-    //document.getElementById("guess-el").innerText = element;
-}
-function guessAdd3() {
-    document.getElementById("guess-el").innerText += 3;
-    //document.getElementById("guess-el").innerText = element;
+function vaultCoder() {
+    let num1 = Math.floor(Math.random() * 3 + 1);
+    let num2 = Math.floor(Math.random() * 3 + 1);
+    let num3 = Math.floor(Math.random() * 3 + 1);
+    let vaultCode = '' + num1 + num2 + num3;
+    return vaultCode;
 }
 
-// let guessedCode = document.getElementById("guess-el").innerText;
-
-let vaultCode = Number(Math.floor(Math.random() * 222 + 111));
-
-if (guessedCode !== vaultCode) {
-    if (guessedCode < vaultCode) {
-        document.getElementById("hint").innerText = "Your number is too low!";
-    }
-    if (guessedCode > vaultCode) {
-        document.getElementById("hint").innerText = "Your number is too high!";
-    }
-    if (guessedCode === vaultCode) {
-        document.getElementById("hint").innerText = "You've unlocked the vault!";
-    }
-
+function playAgain() {
+    guessCounter = 7;
+    localStorage.setItem("guessCounter", guessCounter);
+    document.getElementById("counter-el").innerText = guessCounter;
+    vaultCode = vaultCoder();
+    localStorage.setItem("vaultCode", vaultCode);
+    console.log("New Vault Code:", vaultCode);
+    clearer();
 }
 
-function guessCalc(){
-    if (document.getElementById("guess-el").innerText.length === 3) {
-        var tag = document.getElementById('guess-el');
-        console.log(tag.innerText.length)
-    } else {
-        if (document.getElementById("num1-el")) {
-            guessAdd1();
-        } else if (document.getElementById("num2-el")) {
-            guessAdd2();
-        } else if (document.getElementById("num3-el")) {
-            guessAdd3();
+let vaultCode = vaultCoder();
+localStorage.setItem("vaultCode", vaultCode);
+console.log("Vault Code:", vaultCode);
+
+function guessCalc(value) {
+    let currentGuess = document.getElementById("guess-el").innerText;
+
+    if (currentGuess.length < 3 && guessCounter > 0) {
+        currentGuess += value;
+        document.getElementById("guess-el").innerText = currentGuess;
+
+        if (currentGuess.length === 3) {
+            checkGuess(currentGuess); // Check guess after 3 digits
         }
     }
+}
+
+function checkGuess(guessedCode) {
+    if (guessedCode !== vaultCode) {
+        if (guessedCode < vaultCode) {
+            document.getElementById("hint").innerText = "Your number is too low!";
+        } else if (guessedCode > vaultCode) {
+            document.getElementById("hint").innerText = "Your number is too high!";
+        }
+
+        guessCounter -= 1;
+        localStorage.setItem("guessCounter", guessCounter);
+        document.getElementById("counter-el").innerText = guessCounter;
+
+        if (guessCounter === 0) {
+            document.getElementById("hint").innerText = "Game over! The code was: " + vaultCode;
+            setTimeout(playAgain, 2000);
+        }
+    } else {
+        document.getElementById("hint").innerText = "You've unlocked the vault!";
+    }
+}
+
+function clearer() {
+    document.getElementById("guess-el").innerText = ""; // Clear the displayed guess
+    document.getElementById("hint").innerText = ""; // Clear the hint message
 }
